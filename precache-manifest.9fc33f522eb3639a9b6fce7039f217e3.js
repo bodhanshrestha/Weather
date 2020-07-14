fec039b27dc9c4ee06b54d24c1812bdd4782c6cd
Bodhan Shrestha <bodhanshrestha@gmail.com>
2020-07-14 09:15:55 +0545

    Updates

---
+++ precache-manifest.9fc33f522eb3639a9b6fce7039f217e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592ff0fbcffd4e5423480fe0c16f4e4f",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "9aeade61b1359fea0542",
+    "url": "/Weather/static/css/main.249a9657.chunk.css"
+  },
+  {
+    "revision": "a9d15d762438345c2d20",
+    "url": "/Weather/static/js/2.04f7c404.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/Weather/static/js/2.04f7c404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aeade61b1359fea0542",
+    "url": "/Weather/static/js/main.3b9f9dc0.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  },
+  {
+    "revision": "2d0410c52e9fe2b3e0fb905f129ad19a",
+    "url": "/Weather/static/media/wi-cloudy.2d0410c5.svg"
+  },
+  {
+    "revision": "4b9869a6859846e524d1985de86894f7",
+    "url": "/Weather/static/media/wi-day-sunny.4b9869a6.svg"
+  },
+  {
+    "revision": "ace478e89dad3f20c96b3532b123cfdc",
+    "url": "/Weather/static/media/wi-humidity.ace478e8.svg"
+  },
+  {
+    "revision": "e13be320202dc94ef0d3696c1f508d24",
+    "url": "/Weather/static/media/wi-rain.e13be320.svg"
+  },
+  {
+    "revision": "b9d8d3cf851d2253ea0f306c860c5812",
+    "url": "/Weather/static/media/wi-strong-wind.b9d8d3cf.svg"
+  }
+]);
